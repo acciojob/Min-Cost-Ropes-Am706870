ee93a60f75a7d97c64e884866d307d08e2bc46fb
Am706870 <88397078+Am706870@users.noreply.github.com>
2025-03-09 16:10:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 function mincost(arr)
 { 
+	let minost = 0
+	while (arr.length>1) {
+		arr.sort((a,b)=>a-b);
+		let first = arr.shift();
+		let second = arr.shift();
+		let cost = first + second;
+		mincost += cost;
+		arr.push(cost);
+	}
+	return minost
+	
 //write your code here
 // return the min cost
   
 }
+
 
 module.exports=mincost;
